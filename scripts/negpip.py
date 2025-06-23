3f1f50d43f3b7768403cc761de540f3e7d1b6093
hako-mikan <122196982+hako-mikan@users.noreply.github.com>
2025-06-23 23:44:15 +0900

    #53

--- scripts/negpip.py
+++ scripts/negpip.py
@@ -84,7 +84,7 @@ class Script(modules.scripts.Script):
 
     def ui(self, is_img2img):    
         with InputAccordion(startup_i if is_img2img else startup_t, label=self.title(), visible = not opt_hideui) as active:
-            toggle = gr.Button(elem_id="switch_default", value=f"Toggle startup with Active(Now:{startup_i if is_img2img else startup_t})", variant="primary")
+            toggle = gr.Button(elem_id="switch_default", value=f"Toggle startup with Active(Now:{startup_i if is_img2img else startup_t}), Needs Restart to Apply", variant="primary")
 
         def f_toggle(is_img2img):
             key = NEGPIP_I if is_img2img else NEGPIP_T
